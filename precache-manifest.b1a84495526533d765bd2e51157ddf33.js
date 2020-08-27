1a0993495953868b2c999b488758d50f3f682c8e
Adrian <contact@stroe-adrian.ro>
2020-08-27 15:09:43 +0300

    Updates

---
+++ precache-manifest.b1a84495526533d765bd2e51157ddf33.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7092192e3e5c002a7ae0e3c38da44224",
+    "url": "/react-burger/index.html"
+  },
+  {
+    "revision": "9d5b5e6d0cd4335b7fdf",
+    "url": "/react-burger/static/css/3.fed05348.chunk.css"
+  },
+  {
+    "revision": "ccdd63e3e8a8f3b75851",
+    "url": "/react-burger/static/css/4.51d754bb.chunk.css"
+  },
+  {
+    "revision": "8be7f26b211ba0ab9599",
+    "url": "/react-burger/static/css/5.243e1bc5.chunk.css"
+  },
+  {
+    "revision": "56a6cde56381d91b324f",
+    "url": "/react-burger/static/css/main.c55bf591.chunk.css"
+  },
+  {
+    "revision": "97e99c863bc096b27837",
+    "url": "/react-burger/static/js/2.9d1b0dab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-burger/static/js/2.9d1b0dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5b5e6d0cd4335b7fdf",
+    "url": "/react-burger/static/js/3.8351b169.chunk.js"
+  },
+  {
+    "revision": "ccdd63e3e8a8f3b75851",
+    "url": "/react-burger/static/js/4.c4730b91.chunk.js"
+  },
+  {
+    "revision": "8be7f26b211ba0ab9599",
+    "url": "/react-burger/static/js/5.15733373.chunk.js"
+  },
+  {
+    "revision": "56a6cde56381d91b324f",
+    "url": "/react-burger/static/js/main.4a5532c9.chunk.js"
+  },
+  {
+    "revision": "9a778a7e4e1ee907b169",
+    "url": "/react-burger/static/js/runtime-main.2ab4f326.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
